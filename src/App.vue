3095e6d846448574c8e00fe0d8549a788d937281
Mr_Jing <1633869696@qq.com>
2021-06-15 19:04:02 +0800

    第一次提交VUE代码

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>App组件</h1>
+    <Avatar
+      url="http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c"
+      :size="100"
+    />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script> 
+import Avatar from "./components/Avatar" 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    Avatar,
   }
 }
-</script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script> 
---
+++ src/components/Avatar.vue
@@ -0,0 +1,33 @@
+<template>
+  <img 
+    class="avatar-img"
+    :src="url" 
+    :style="{
+        width: size +'px',
+        height:size + 'px'
+    }"
+   />
+</template>
+
+<script>
+export default {
+    props:{
+        url: {
+            type:String,
+            required:true
+        },
+        size:{
+            type:Number
+        }
+    }
+}
+</script>
+
+<style scoped >
+   .avatar-img{
+       border-radius: 50%;
+       object-fit: cover;
+       display: block;
+   }
+
+</style>
