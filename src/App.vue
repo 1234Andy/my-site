c310563509d259d5674b8288e0b0e3b6727d8e2b
Mr_Jing <1633869696@qq.com>
2021-06-19 22:52:52 +0800

    完成icon组件

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,18 @@
       url="http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c"
       :size="100"
     />
+    <Icon type="home"/>
   </div>
 </template>
 
 <script> 
-import Avatar from "./components/Avatar" 
+import Avatar from "./components/Avatar";
+import Icon from "./components/Icon"
 export default {
   name: 'App',
   components: { 
     Avatar,
+    Icon
   }
 }
 </script> 
